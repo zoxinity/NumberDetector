ea0a9e9214a7f4e3244d6ff175e532559150f1f7
Noue Zoxinity <zoxinity@gmail.com>
2020-11-25 02:07:49 +0300

    Initial commit

---
+++ src/NumberDetector.cpp
@@ -0,0 +1,32 @@
+﻿#include "DrawText.hpp"
+
+int main()
+{
+	std::cout << "Built with OpenCV " << CV_VERSION << std::endl;
+	cv::Mat image;
+	cv::VideoCapture capture;
+	capture.open(0);
+	if(capture.isOpened())
+	{
+		std::cout << "Capture is opened" << std::endl;
+		for(;;)
+		{
+			capture >> image;
+			if(image.empty())
+				break;
+			drawText(image);
+			imshow("Sample", image);
+			if(cv::waitKey(10) >= 0)
+				break;
+		}
+	}
+	else
+	{
+		std::cout << "No capture" << std::endl;
+		image = cv::Mat::zeros(480, 640, CV_8UC1);
+		drawText(image);
+		cv::imshow("Sample", image);
+		cv::waitKey(0);
+	}
+	return 0;
+}
---
+++ src/drawText/DrawText.hpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <iostream>
+
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+
+void drawText(cv::Mat &image);

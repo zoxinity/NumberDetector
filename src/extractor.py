534dab3882f0528ec83ab949aaa5669911756cc8
Noue Zoxinity <zoxinity@gmail.com>
2020-11-27 02:05:51 +0300

    Подчистил репо после мержа

--- src/extractor.py
+++ src/extractor.py
@@ -1,36 +1,36 @@
 import numpy as np
-import cv2 as cv
+from cv2 import cv2
 
 def imshow(winname,mat):
-    cv.imshow(winname,mat)
-    cv.waitKey(0)
+    cv2.imshow(winname,mat)
+    cv2.waitKey(0)
 
 def main():
     # загрузка изображения
-    path = "D:\\prog\\repos\\NumberDetector\\samples\\"
+    path = "D:\\prog\\repos\\NumberDetector\\resources\\"
     sample = "sample1.jpg"
-    im_color = cv.imread(path+sample,cv.IMREAD_COLOR)
-    im = cv.imread(path+sample,cv.IMREAD_GRAYSCALE)
+    im_color = cv2.imread(path+sample,cv2.IMREAD_COLOR)
+    im = cv2.imread(path+sample,cv2.IMREAD_GRAYSCALE)
     imshow("im",im)
     # перевод в бинарное изображение
-    blur = cv.GaussianBlur(im,(11,11),0)
-    thresh = cv.adaptiveThreshold(blur,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,33,20)
+    blur = cv2.GaussianBlur(im,(11,11),0)
+    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,33,20)
     imshow("thresh",thresh)
     # удаление шума
     opening_kernel = np.ones((5,5),np.uint8)
-    opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,opening_kernel)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,opening_kernel)
     imshow("opening",opening)
     # склейка отделившихся частей цифр
     closing_kernel = np.ones((21,21),np.uint8)
-    closing = cv.morphologyEx(opening,cv.MORPH_CLOSE,closing_kernel)
+    closing = cv2.morphologyEx(opening,cv2.MORPH_CLOSE,closing_kernel)
     imshow("closing",closing)
     # контуры отдельных цифр
     # используются только контуры верхнего уровня, так как цифры могут иметь топологию, отличную от прямой
-    contours,hierarchy = cv.findContours(closing,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+    contours,hierarchy = cv2.findContours(closing,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     im_rects = im_color
     for cnt in contours:
-        [x,y,w,h] = cv.boundingRect(cnt)
-        cv.rectangle(im_rects,(x,y),(x+w,y+h),(0,0,255),2)
+        [x,y,w,h] = cv2.boundingRect(cnt)
+        cv2.rectangle(im_rects,(x,y),(x+w,y+h),(0,0,255),2)
     imshow("im_rects",im_rects)
 
 if __name__ == "__main__":

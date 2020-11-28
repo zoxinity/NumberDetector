2ffb1abe6cfbf67463de9f6cf3565e0f9e6398a0
Данила <guseffdanila@ya.ru>
2020-11-28 14:17:37 +0300

    include training in main

--- main.py
+++ main.py
@@ -6,27 +6,14 @@ import argparse
 
 
 from src.detectDigits import detectDigits
+from src.train import train
 
 
-def main():
-
-    parser = argparse.ArgumentParser(
-        description='Program for detection write digits')
-    parser.add_argument('files', metavar='path', type=str, nargs='*',
-                        help='files for processing',
-                        default='resources/img.jpg')
-    parser.add_argument('-t', '--train', dest='is_train', action='store_true',
-                        help='set this flag if it is training stage')
-    parser.add_argument('-m', '--model', dest='model_file',
-                        default="resources/knn_clf.sav", type=str,
-                        help='file with parametrs for model, would be '
-                             'updated for training or read for prediction')
-    args = parser.parse_args()
+def predict(files, knn_clf):
 
     cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
 
-    # todo add processing of many files
-    inImage = cv2.imread(args.files[0], cv2.IMREAD_GRAYSCALE)
+    inImage = cv2.imread(files[0], cv2.IMREAD_GRAYSCALE)
 
     # detect digits on the image
     digits, digitsCoords = detectDigits(inImage)
@@ -37,12 +24,6 @@ def main():
         cv2.imshow("lol", digit)
         cv2.waitKey(0)
         digit = digit.reshape((1, 28*28))
-
-        # load model and make prediction
-        try:
-            knn_clf = pickle.load(open(args.model_file, 'rb'))
-        except OSError:
-            sys.exit('Please, train model (or add model file to ./resources/)')
 
         prediction = str(knn_clf.predict(digit))
 
@@ -61,6 +42,34 @@ def main():
     cv2.waitKey(0)
 
     cv2.destroyAllWindows()
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='Program for detection write digits')
+    parser.add_argument('files', metavar='path', type=str, nargs='*',
+                        help='files for processing',
+                        default='resources/img.jpg')
+    parser.add_argument('-t', '--train', dest='is_train', action='store_true',
+                        help='set this flag if it is training stage')
+    parser.add_argument('-m', '--model', dest='model_file',
+                        default="resources/knn_clf.sav", type=str,
+                        help='file with parametrs for model, would be '
+                             'updated for training or read for prediction')
+    args = parser.parse_args()
+
+    if args.is_train:
+        train(args.model_file)
+    else:
+        # load model and make prediction
+        try:
+            knn_clf = pickle.load(open(args.model_file, 'rb'))
+        except OSError:
+            sys.exit(f"Can't open model file by path '{args.model_file}'"
+                     "\nPlease, train model (or set correct path)")
+        for line in args.fiels:
+            predict(line, knn_clf)
 
 
 if __name__ == '__main__':

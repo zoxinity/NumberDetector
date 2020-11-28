da0ff95d8aec17dbc8f0d2d331886e576095c2eb
Noue Zoxinity <zoxinity@gmail.com>
2020-11-28 11:28:00 +0300

    Слил файлы поиска чисел

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ from src.detectDigits import detectDigits
 
 cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
 
-try:
-    inImage = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
-except IndexError:
-    sys.exit('Please, select image')
-# inImage = cv2.imread("resources/img.jpg", cv2.IMREAD_GRAYSCALE)
+# try:
+#     inImage = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
+# except IndexError:
+#     sys.exit('Please, select image')
+inImage = cv2.imread("resources/img.jpg", cv2.IMREAD_GRAYSCALE)
 
 # detect digits on the image
 digits, digitsCoords = detectDigits(inImage)
 
 for (digit, coords) in zip(digits, digitsCoords):
-    # invert and normalize image
-    digit = 255 - digit
+    # normalize image
     digit = digit/255.0
+    cv2.imshow("lol", digit)
+    cv2.waitKey(0)
     digit = digit.reshape((1, 28*28))
     
     # load model and make prediction
--- src/detectDigits.py
+++ src/detectDigits.py
@@ -1,17 +1,68 @@
 from cv2 import cv2
 import numpy as np
 
-def detectDigits(inImage):
-    ret, inImage = cv2.threshold(inImage, 127, 255, cv2.THRESH_BINARY)
 
-    kernel = np.ones((5,5),np.uint8)
-    inImage = cv2.erode(inImage,kernel,iterations = 1)
-    # array of images with every single digit
-    res = [
-        cv2.resize(inImage[81:151, 160:230], (28, 28)),
-        cv2.resize(inImage[84:154, 320:390], (28, 28)),
-        cv2.resize(inImage[85:165, 480:550], (28, 28))
-    ]
-    coords = [(160, 81), (320, 84), (480, 85)]
+def imshow(winname, mat):
+    cv2.imshow(winname, mat)
+    cv2.waitKey(0)
+
+
+def crop(img, rect, size):
+    [x, y, w, h] = rect
+    xc, yc = x+w//2, y+h//2
+    s = 0
+    if h > w:
+        s = h
+    else:
+        s = w
+    xc, yc = xc-s//2, yc-s//2
+    coord = xc, yc
+    crop = cv2.resize(img[yc:yc+s, xc:xc+s], size)
+    return crop, coord
+
+
+def detectDigits(inImage):
+    # перевод в бинарное изображение
+    blur = cv2.GaussianBlur(inImage, (11, 11), 0)
+    thresh = cv2.adaptiveThreshold(
+        blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 33, 20)
+    imshow("1", thresh)
+
+    # удаление шума
+    opening_kernel = np.ones((5, 5), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, opening_kernel)
+
+    # склейка отделившихся частей цифр
+    closing_kernel = np.ones((21, 21), np.uint8)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, closing_kernel)
+    imshow("2", closing)
+    
+    # контуры отдельных цифр
+    # используются только контуры верхнего уровня, так как цифры могут иметь топологию, отличную от прямой
+    contours, hierarchy = cv2.findContours(
+        closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    res = []
+    coords = []
+    for cnt in contours:
+        [x, y, w, h] = cv2.boundingRect(cnt)
+        cropImg, coord = crop(closing, (x,y,w,h), (28,28))
+        res.append(cropImg)
+        coords.append(coord)
 
     return res, coords
+
+
+def main():
+    # загрузка изображения
+    path = "D:\\prog\\repos\\NumberDetector\\resources\\"
+    sample = "sample1.jpg"
+    inImage = cv2.imread(path+sample, cv2.IMREAD_GRAYSCALE)
+
+    digits, digitsCoords = detectDigits(inImage)
+    for (digit, coords) in zip(digits, digitsCoords):
+        cv2.imshow("digit", digit)
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
